9bcb7abbfd40361c5382308de586e5ecb5dffde9
enuggetry <github@meyao.net>
2016-10-16 01:24:18 -0700

    update

--- vtx.php
+++ vtx.php
@@ -4,13 +4,20 @@
 	$hstr = $_GET['h'];	// height
 	$h = intval($hstr);
 	
-	$img = imagecreatetruecolor($h,20);
+	// replace _ with space
+	$txt = str_replace("_"," ",$txt);
+	
+	// prepare transparent canvas
+	$img = imagecreatetruecolor(150,20);
 	imagesavealpha($img, true);
 	$bkg = imagecolorallocatealpha($img, 0, 0, 0, 127);
 	imagefill($img, 0, 0, $bkg);
-
+	
+	// paint text
 	$txtclr = imagecolorallocate($img, 0, 0, 255);
 	imagestring($img, 5, 0, 0, $txt, $txtclr);
+	
+	// rotate image
 	$r_img = imagerotate($img, 90, $bkg,1);
 	imagealphablending($r_img,false);
 	imagesavealpha($r_img,true);

5654fd607c2a7b34de76883d17721875427dc890
enuggetry <github@meyao.net>
2016-10-16 14:45:29 -0700

    add TTF capability.

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+	$txt = $_GET['tx'];	// text string
+	$hstr = $_GET['h'];	// height
+	$h = intval($hstr);
+	
+	// Establish image factors:
+	//$text = 'Sample space';
+	$font_size = 12; // Font size is in pixels.
+	$font_file = './fonts/AUBREY1__.TTF'; // This is the path to your font file.
+
+	// Retrieve bounding box:
+	$type_space = imagettfbbox($font_size, 0, $font_file, $txt);
+
+	// Determine image width and height, 10 pixels are added for 5 pixels padding:
+	$img_w = abs($type_space[4] - $type_space[0]) + 10;
+	$img_h = abs($type_space[5] - $type_space[1]) + 10;
+	
+	$x = 5; // Padding of 5 pixels.
+	$y = $img_h - 5; // So that the text is vertically centered.
+	
+	
+	// replace _ with space
+	$txt = str_replace("_"," ",$txt);
+	
+	// prepare transparent canvas
+	$img = imagecreatetruecolor($img_w,$img_h);
+	imagesavealpha($img, true);
+	$bkg = imagecolorallocatealpha($img, 0, 0, 0, 127);
+	imagefill($img, 0, 0, $bkg);
+	
+	// paint text
+	$txtclr = imagecolorallocate($img, 0, 0, 255);
+	//imagestring($img, 5, 5, 0, $txt, $txtclr);
+	imagettftext($img, $font_size, 0, $x, $y, $tx_color, $font_file, $txt);	
+	// rotate image
+	$r_img = imagerotate($img, 90, $bkg,1);
+	imagealphablending($r_img,false);
+	imagesavealpha($r_img,true);
+
+	header('Content-type: image/png');
+	imagepng($r_img);
+	imagedestroy( $img );	
+	imagedestroy( $r_img );	
+	
+?>

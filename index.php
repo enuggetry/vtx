7b99d81bb2e423d6981155fd38f20b6340e71475
enuggetry <github@meyao.net>
2016-10-16 17:55:53 -0700

    Add font= and size= option

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
 	$txt = $_GET['tx'];	// text string
 	
 	// Establish image factors:
-	//$text = 'Sample space';
 	$font_size = 12; // Font size is in pixels.
-	$font_file = './fonts/DroidSans.ttf'; // This is the path to your font file.
+	$font = './fonts/DroidSans.ttf'; // This is the path to your font file.
+	if (isset($_GET['font'])) {
+		$font = './fonts/'.$_GET['font'].'.ttf';
+	}
+	if (isset($_GET['size'])) {
+		$font_size = intval($_GET['size']);
+	}
 
 	// Retrieve bounding box:
-	$type_space = imagettfbbox($font_size, 0, $font_file, $txt);
+	$type_space = imagettfbbox($font_size, 0, $font, $txt);
 
 	// Determine image width and height, 10 pixels are added for 5 pixels padding:
 	$img_w = abs($type_space[4] - $type_space[0]) + 10;
@@ -29,7 +34,7 @@
 	// paint text
 	$txtclr = imagecolorallocate($img, 0, 0, 255);
 	//imagestring($img, 5, 5, 0, $txt, $txtclr);
-	imagettftext($img, $font_size, 0, $x, $y, $tx_color, $font_file, $txt);	
+	imagettftext($img, $font_size, 0, $x, $y, $tx_color, $font, $txt);	
         
 	// rotate image
 	$r_img = imagerotate($img, 90, $bkg,1);
